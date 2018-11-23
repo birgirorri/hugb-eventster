3069d6c3db2e667c18dce0bc9b76bc18fb15612a
Birgir Orri <boo11@hi.is>
2018-11-23 21:39:04 +0000

    smá fix

--- SpringBootMVC/src/main/java/project/service/EventService.java
+++ SpringBootMVC/src/main/java/project/service/EventService.java
@@ -9,51 +9,20 @@ import project.persistence.repositories.*;
 import java.util.Collections;
 import java.util.List;
 
-@Service
+public interface EventService{
+        
+    public Event createEvent(Event event);
 
-public class EventService{
-	
-	
-	 EventRepository repository;
-	
-	 
-	 @Autowired
-	    public EventService(EventRepository repository) {
-	        this.repository = repository;
-	    }
-	 
-
-	    public Event createEvent(Event event) {
-	        return repository.save(event);
-	    }
-
-	    
-	    public void delete(Event event) {
-	        repository.delete(event);
-	    }
-	    
-	    public List<Event> findAllEvents() {
-	        return repository.findAll();
-	    }
-	    
-	    public Event findEventByID(Long eventID){
-	    	return repository.findOne(eventID);
-	    }
-	    
-	    public List<Event> findEventByName(String name){
-	    	return repository.findByEventName(name);
-	    }
-	    
-	    /*public List<Group> findUserByGroup(int GroupID) {
-	    	return repository.findGroupByID(groupID);
-	    }
-	    */
-	    
-	    public void updateUser(Event event){
-	    	
-	    }
-	    
-	    public List<Event> sortEvent(String by, List<Event> events){
-	    	return events;
-	    }
+    
+    public void delete(Event event);
+    
+    public List<Event> findAllEvents();
+    
+    public Event findEventByID(Long eventID);
+    
+    public List<Event> findEventByName(String name);
+    
+    public void updateUser(Event event);
+    
+    public List<Event> sortEvent(String by, List<Event> events);
 }
