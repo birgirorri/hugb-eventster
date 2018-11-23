52151bcf385ad472607fd690f254ce0b8de725e6
Birgir Orri <boo11@hi.is>
2018-11-23 21:37:54 +0000

    LAGA ÞAÐ SEM ADAM GERÐI

---
+++ SpringBootMVC/src/main/java/project/service/Implementation/EventServiceImplementation.java
@@ -0,0 +1,66 @@
+package project.service.Implementation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import project.persistence.entities.Event;
+import project.persistence.repositories.*;
+import project.service.EventService;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class EventServiceImplementation implements EventService{
+	
+	
+	 EventRepository repository;
+	
+	 
+	 @Autowired
+	    public EventService(EventRepository repository) {
+	        this.repository = repository;
+	    }
+	 
+
+		@Override
+	    public Event createEvent(Event event) {
+	        return repository.save(event);
+	    }
+
+		
+		@Override
+	    public void delete(Event event) {
+	        repository.delete(event);
+	    }
+		
+		@Override
+	    public List<Event> findAllEvents() {
+	        return repository.findAll();
+	    }
+		
+		@Override
+	    public Event findEventByID(Long eventID){
+	    	return repository.findOne(eventID);
+	    }
+		
+		@Override
+	    public List<Event> findEventByName(String name){
+	    	return repository.findByEventName(name);
+	    }
+	    
+	    /*public List<Group> findUserByGroup(int GroupID) {
+	    	return repository.findGroupByID(groupID);
+	    }
+	    */
+		
+		@Override
+	    public void updateUser(Event event){
+	    	
+	    }
+		
+		@Override
+	    public List<Event> sortEvent(String by, List<Event> events){
+	    	return events;
+	    }
+}
---
+++ SpringBootMVC/src/main/java/project/service/Implementation/GroupServiceImplementation.java
@@ -0,0 +1,95 @@
+package project.service.Implementation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import project.persistence.entities.PostitNote;
+import project.persistence.entities.User;
+import project.persistence.entities.Event;
+import project.persistence.entities.Group;
+import project.persistence.repositories.PostitNoteRepository;
+import project.persistence.repositories.GroupRepository;
+import project.service.GroupService;
+import project.service.PostitNoteService;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class GroupServiceImplementation implements GroupService{
+	
+	
+	 GroupRepository repository;
+	
+	 
+	 @Autowired
+	public GroupService(GroupRepository repository) {
+	    this.repository = repository;
+	}
+	
+	/*
+	public Group createUser(Group group) {
+	    return repository.save(group);
+	}
+	*/   
+	@Override
+	public Group addGroup(Group group) {
+		System.out.println(group.getGroupName() +"=============================" + group.getGroupInfo() );
+		
+		
+		return repository.save(group);
+	}
+	
+	@Override
+	public void delete(Group group) {
+	    repository.delete(group);
+	}
+	
+	@Override
+	public List<Group> findAllGroups() {
+		System.out.println("finding groups============================");
+	    return repository.findAll();
+	}
+	/*  
+	public User findUserByID(Long userID){
+		return repository.findOne(userID);
+	}
+	*/
+	
+	@Override
+	public Group findGroupByID(Long ID){
+		return repository.findOne(ID);
+	}
+	
+	@Override
+	public List<Group> findByName(String name){
+		return repository.findByName(name);
+	}
+	
+	
+	/*public List<Group> findUserByGroup(int GroupID) {
+		return repository.findGroupByID(groupID);
+	}
+	*/
+	
+	@Override
+	public void updateUser(User user){
+		
+	}
+	
+	@Override
+	public List<User> sortUser(String by, List<User> users){
+		return users;
+	}
+	
+	
+	@Override
+	public List<User> findAllUsersInGroup(String groupName){
+		return repository.findGroupMembers(groupName);
+	}
+	
+	@Override
+	public void addMember(String username,String email, String groupName) {
+		repository.save( new Group(username,email,groupName));
+	}
+}
